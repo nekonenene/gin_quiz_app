f817ad1ed267ed00892f076cce9eb1aba6166c84
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-04-28 20:08:27 +0900

    メソッドで外出し

--- user/router.go
+++ user/router.go
@@ -9,34 +9,38 @@ import (
 
 // UserRouter : router
 func UserRouter(router *gin.RouterGroup) {
-	router.GET("/list", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "User List!",
-		})
+	router.GET("/list", listUser)
+	router.GET("/show/:id", showUserByID)
+	router.POST("/create", createUser)
+}
+
+func listUser(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "User List!",
 	})
+}
 
-	router.GET("/show/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		idInt, _ := strconv.Atoi(id)
-		user, err := FindByID(uint(idInt))
-		if err != nil {
-			c.JSON(500, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(200, gin.H{
-			"message": "User name: " + user.Name,
+func showUserByID(c *gin.Context) {
+	id := c.Param("id")
+	idInt, _ := strconv.Atoi(id)
+	user, err := FindByID(uint(idInt))
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
 		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "User name: " + user.Name,
 	})
+}
 
-	router.POST("/create", func(c *gin.Context) {
-		var user User
-		c.BindJSON(&user)
+func createUser(c *gin.Context) {
+	var user User
+	c.BindJSON(&user)
 
-		c.JSON(200, gin.H{
-			"message": "Created user: " + user.Name,
-		})
+	c.JSON(200, gin.H{
+		"message": "Created user: " + user.Name,
 	})
 }
