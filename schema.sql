2fd2bab6cb8be9e84b8f9d24d12247ec79e4d384
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-05-02 11:55:02 +0900

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,20 @@
+CREATE TABLE `session` (
+  `session_id` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `data` text COLLATE utf8mb4_bin NOT NULL,
+  `created_at` datetime(3) NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `created_at_idx` (`created_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 CREATE TABLE `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `provider` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `provider_id` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `is_admin` tinyint(1) DEFAULT NULL,
   `created_at` datetime(3) NOT NULL,
   `updated_at` datetime(3) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `openid_idx` (`provider`,`provider_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
