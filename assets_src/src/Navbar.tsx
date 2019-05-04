22c165be180310aa8c2fc2d0b9079ce52cd1629b
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-05-05 04:08:17 +0900

    Create and use NavbarMenu.tsx

--- assets_src/src/Navbar.tsx
+++ assets_src/src/Navbar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import NavbarMenu from './NavbarMenu';
 import User from './model/User';
 
 export interface Props {
@@ -16,28 +17,26 @@ export default class Navbar extends React.Component<Props, {}> {
   }
 
   public render() {
-    if (this.props.user.id > 0) {
-      return (
-        <nav className="red accent-3" role="navigation">
-          <div className="nav-wrapper container">
-            <a href="/" className="brand-logo">Quiz App</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="/signout">ログアウト</a></li>
-            </ul>
-          </div>
-        </nav>
-      );
-    } else {
-      return (
-        <nav className="red accent-3" role="navigation">
-          <div className="nav-wrapper container">
-            <a href="/" className="brand-logo">Quiz App</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="/oauth/google/signin">ログイン</a></li>
-            </ul>
-          </div>
-        </nav>
-      );
-    }
+    const isSignin = this.props.user.id > 0
+    return (
+      <nav className="red accent-3" role="navigation">
+        <div className="nav-wrapper container">
+          <a href="/" className="brand-logo">Quiz App</a>
+          <ul className="right hide-on-small-and-down">
+            <NavbarMenu
+              isSignin={isSignin}
+            />
+          </ul>
+          <ul className="right hide-on-med-and-up">
+            <li><a className="dropdown-trigger show-on-small" href="#!" data-target="menuDropdown"><i className="material-icons">menu</i></a></li>
+          </ul>
+          <ul className="dropdown-content" id="menuDropdown">
+            <NavbarMenu
+              isSignin={isSignin}
+            />
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
---
+++ assets_src/src/NavbarMenu.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+export interface Props {
+  isSignin: boolean;
+}
+
+export default class NavbarMenu extends React.Component<Props, {}> {
+  constructor(props: any) {
+    super(props)
+  }
+
+  public render() {
+    const signinElement = this.props.isSignin ?
+      <li><a href="/signout">ログアウト</a></li> :
+      <li><a href="/oauth/google/signin">ログイン</a></li>
+    return (
+      signinElement
+    );
+  }
+}
