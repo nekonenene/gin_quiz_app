d2930e1a75a9d1ba3baec927a25040be200e70d2
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-05-05 03:15:03 +0900

    Remove home.html

--- root/router.go
+++ root/router.go
@@ -1,35 +1,17 @@
 package root
 
 import (
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 	"github.com/nekonenene/gin_quiz_app/session"
 )
 
 func RootRouter(router *gin.RouterGroup) {
 	router.GET("/", root)
-	router.GET("/home", home)
 	router.GET("/signout", signout)
 }
 
 func root(c *gin.Context) {
-	if session.IsSignin(c) {
-		c.Redirect(302, "/home")
-	}
-
 	c.HTML(200, "index.html", gin.H{})
-}
-
-func home(c *gin.Context) {
-	userID, err := session.CurrentUserID(c)
-	if err != nil {
-		c.Redirect(302, "/")
-	}
-
-	c.HTML(200, "home.html", gin.H{
-		"userID": strconv.FormatUint(userID, 10),
-	})
 }
 
 func signout(c *gin.Context) {
