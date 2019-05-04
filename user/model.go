c9919598dbd0d2b05ef963b78d18d778ca56b287
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2019-05-05 08:57:08 +0900

    Remove unused methods

--- user/model.go
+++ user/model.go
@@ -11,9 +11,9 @@ type User struct {
 	Provider   string    `gorm:"not null; unique_index:openid_idx"  json:"-"`
 	ProviderID string    `gorm:"not null; unique_index:openid_idx"  json:"-"`
 	Name       string    `gorm:"not null"                           json:"name"        binding:"required,max=255"`
-	Email      string    `gorm:"not null"                           json:"email"       binding:"required,max=255"`
-	IsAdmin    bool      `                                          json:"is_admin"`
-	CreatedAt  time.Time `gorm:"type:datetime(3); not null"         json:"created_at"`
+	Email      string    `gorm:"not null"                           json:"email"       binding:"required,max=255,email"`
+	IsAdmin    bool      `                                          json:"-"`
+	CreatedAt  time.Time `gorm:"type:datetime(3); not null"         json:"-"`
 	UpdatedAt  time.Time `gorm:"type:datetime(3); not null"         json:"-"`
 }
 
--- user/router.go
+++ user/router.go
@@ -1,8 +1,6 @@
 package user
 
 import (
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 	"github.com/nekonenene/gin_quiz_app/common"
 	"github.com/nekonenene/gin_quiz_app/session"
@@ -12,8 +10,6 @@ func UserRouter(router *gin.RouterGroup) {
 	router.GET("/current", currentUser)
 	router.POST("/update", updateUser)
 	router.GET("/list", listUser)
-	router.GET("/show/:id", showUserByID)
-	router.POST("/create", createUser)
 }
 
 func currentUser(c *gin.Context) {
@@ -86,32 +82,4 @@ func listUser(c *gin.Context) {
 	}
 
 	common.OkResponse(c, "users", users)
-}
-
-func showUserByID(c *gin.Context) {
-	id := c.Param("id")
-	uid, err := strconv.ParseUint(id, 10, 64)
-	if err != nil {
-		common.ErrorResponse(c, "id must be integer")
-		return
-	}
-
-	user, err := FindByID(uid)
-	if err != nil {
-		common.ErrorResponse(c, err.Error())
-		return
-	}
-
-	common.OkResponse(c, "user", user)
-}
-
-func createUser(c *gin.Context) {
-	var user User
-	if err := c.ShouldBindJSON(&user); err != nil {
-		common.BadRequestErrorResponse(c, err.Error())
-		return
-	}
-	user.Create()
-
-	common.OkResponse(c, "user", user)
 }
